14874dc94703366552a1d537a579b553cfc827dc
Culver Stone <c_stone157@hotmail.com>
2022-10-07 21:02:19 -0700

    Added log4j, and started building the rest of the service

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var fs = require('fs');
 var obj = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
+const log4js = require("log4js");
+
+log4js.configure({
+  appenders: { console: { type: "console" } },
+  categories: { default: { appenders: ["console"], level: "info" } },
+});
+
+var logger = log4js.getLogger();
+
 // Sleep for a specified number of milliseconds
 function sleep(ms) {
   return new Promise((resolve) => {
@@ -11,26 +20,26 @@ function sleep(ms) {
 // Log our messages
 function logMessage(process, msg) {
   if (msg !== undefined)
-    console.error('[' + obj.user + '][' + obj.role + '][' + process + '] ' + msg);
+    logger.info('[' + obj.user + '][' + obj.role + '][' + process + '] ' + msg);
   else
-    console.error('[' + obj.user + '][' + obj.role + '][' + process + ']');
+    logger.info('[' + obj.user + '][' + obj.role + '][' + process + ']');
 }
 
 async function start() {
   // If there is a wait to login the first time
   if (obj.firstLoginInterval !== undefined) {
-    await sleep(Math.random() * obj.firstLoginInterval);
+    await sleep((obj.minFirstLoginInterval !== undefined) ? (obj.minFirstLoginInterval + Math.random() * obj.firstLoginInterval) :
+                                                            (Math.random() * obj.firstLoginInterval));
   }
-  logMessage("login(1)");
+  logMessage("login");
   obj.loggedIn = true;    // Set logged in to true
 
   
   // Run non-stop
   while (true) {
     // Wait to run our commands
-    await sleep(Math.random() * obj.interval);
-
-    console.log(" ------------- Looping -----------------");
+    await sleep((obj.minInterval !== undefined) ? (obj.minInterval + Math.random() * obj.interval) :
+                                                  (Math.random() * obj.interval));
     
     // If the User isn't logged in then deal with that
     // If we have a defined probablity of Logging in, then go ahead and check to see if they log back in
@@ -39,16 +48,28 @@ async function start() {
       logMessage('login');
     
       // If this user is defined as being able to logout, then do that
-    } else if (obj.logoutOdds !== undefined && obj.loginOdds > Math.random()) { 
+    } else if (obj.logoutOdds !== undefined && obj.loginOdds > Math.random()) {
+      // Check to see if we have any processes still running before logging out (I'm pretty sure this will break the graph)
+      for (let i=0; i < obj.process.length; i++) {
+        let p = obj.process[i];
+        if (obj[p].started === true){
+          obj[p].started = false;
+          logMessage(p, obj[p].started ? 'Start': 'Stop');
+          
+          // Wait the minimum time and then keep killing processes
+          await sleep((obj.minInterval !== undefined) ? (obj.minInterval + Math.random() * obj.interval) :
+                                                        (Math.random() * obj.interval));
+        }
+      }
+      
       obj.loggedIn = false;
       logMessage('logout')
-    
+        
       // Our or else process messages
     } else { 
       let n = Math.floor(obj.process.length * Math.random());
       let p = obj.process[n]
       
-console.log(p + ": obj." + p + " === " + obj.p);
       
       if (!obj[p].started){
         obj[p].started = true;
@@ -56,17 +77,18 @@ console.log(p + ": obj." + p + " === " + obj.p);
         obj[p].started = false;
       }
       
-      logMessage(p, obj[p].started);
+      logMessage(p, obj[p].started ? 'Start': 'Stop');
     }
   }
 }
 
+
 // Before we start our application, create all of our process (if they don't already exist)
-for (let i = 0; i++; i < obj.process.length) {
+for (let i = 0; i < obj.process.length; i++) {
   if (obj[obj.process[i]] === undefined)
     obj[obj.process[i]] = {};
   obj[obj.process[i]].started = false;
 }
-  
+
 
 start();
